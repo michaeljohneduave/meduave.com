7421bd93a4d83821849e153a3a5c21750c3d2f9c
Michael Eduave <michaeljohneduave@gmail.com>
2023-09-13 12:00:19 -0700

    Move js code to script tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
+  output: "hybrid",
   adapter: vercel()
 });
