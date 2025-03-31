5e271ea9d09aa44b2913a7c02007360e0628eb5a
Michael <michaeljohneduave@gmail.com>
2025-03-30 19:02:49 -0700

    Add cf turnstile

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,8 @@ interface ImportMetaEnv {
   readonly PROD: boolean;
   readonly KV_REST_API_URL: string;
   readonly KV_REST_API_TOKEN: string;
+  readonly TURNSTILE_SITE_KEY: string;
+  readonly TURNSTILE_SECRET_KEY: string;
 }
 
 interface ImportMeta {
